e2970f0fbcf35eecb40b9c18bf1101681c0b5786
Ryan Posener <rposener@practice-engine.com>
2018-08-03 16:36:10 -0600

    Work towards prepping for QuickFee Receipting (handling of bankfiles and additional request to redplanet via ReceiptService/ReceiptSync

--- src/PEPaymentProvider.QuickFee/QuickFeeProvider.cs
+++ src/PEPaymentProvider.QuickFee/QuickFeeProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PEPaymentProvider.Receipting;
+using PEPaymentProvider.RedPlanet;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -13,6 +15,13 @@ namespace PEPaymentProvider
 {
     public class QuickFeeProvider : IPaymentProvider
     {
+        private readonly RedPlanetXML redPlanetXML;
+
+        public QuickFeeProvider()
+        {
+            redPlanetXML = new RedPlanetXML();
+        }
+
         /// <summary>
         /// Display Name of the Provider
         /// </summary>
@@ -51,17 +60,17 @@ namespace PEPaymentProvider
                 httpClient.BaseAddress = new Uri(config.Url);
 
                 // Login the the API
-                var loginXML = buildLoginRequest(config);
+                var loginXML = redPlanetXML.BuildLoginRequest(config);
                 XDocument loginResult = await PostToQuickFee(httpClient, loginXML);
-                if (!IsSuccess(loginResult))
-                    return GetError(loginResult);
+                if (!redPlanetXML.IsSuccess(loginResult))
+                    return redPlanetXML.GetError(loginResult);
 
                 // Request a New Quote
-                var session = GetSession(loginResult);
-                string quoteXML = buildQuoteRequest(session, client, invoices);
+                var session = redPlanetXML.GetSession(loginResult);
+                string quoteXML = redPlanetXML.BuildQuoteRequest(session, client, invoices);
                 XDocument quoteResult = await PostToQuickFee(httpClient, quoteXML);
-                if (!IsSuccess(quoteResult))
-                    return GetError(quoteResult);
+                if (!redPlanetXML.IsSuccess(quoteResult))
+                    return redPlanetXML.GetError(quoteResult);
 
                 // Logoff the API
                 //var logoffXML = buildLogoffRequest(session);
@@ -71,7 +80,7 @@ namespace PEPaymentProvider
                 //    return GetError(logoffResult);
 
                 // Return the Redirect
-                return GetRedirect(quoteResult);
+                return redPlanetXML.GetRedirect(quoteResult);
             }
             catch (ConfigurationErrorsException cex)
             {
@@ -88,78 +97,6 @@ namespace PEPaymentProvider
         }
 
         /// <summary>
-        /// Builds the FundML Quote XML Request
-        /// </summary>
-        /// <param name="session">Session ID to Use</param>
-        /// <param name="client"></param>
-        /// <param name="invoices"></param>
-        /// <returns></returns>
-        private string buildQuoteRequest(string session, ClientDetails client, IEnumerable<InvoiceDetails> invoices)
-        {
-            var invtotal = invoices.Select(i => i.DebtForUnpaid).Sum();
-            var invnumbers = String.Join(",", invoices.Select(i => i.DebtTranRefAlpha).ToArray());
-
-            var linebreaks = @"[\r\n]+";
-            var regex = new Regex(linebreaks);
-            var addrlines = regex.Split(client.ContAddress);
-            string addr1 = null;
-            string addr2 = null;
-            if (addrlines.Count() > 0)
-                addr1 = addrlines[0];
-            if (addrlines.Count() > 1)
-                addr2 = addrlines[1];
-
-            var request = new XDocument(
-                new XElement("FundML",
-                new XElement("Version", 1),
-                new XElement("Request",
-                new XElement("Code", "NewQuote"),
-                new XElement("Session", session),
-                new XElement("Client",
-                new XElement("ABN", client.ClientGovCode),
-                new XElement("Name", client.ClientName),
-                new XElement("RegisteredName", client.ClientName),
-                new XElement("BrokerID", client.ContIndex),
-                new XElement("PostalAddress",
-                new XElement("Address1", addr1),
-                new XElement("Address2", addr2),
-                new XElement("Suburb", client.ContTownCity),
-                new XElement("State", client.ContCounty),
-                new XElement("Postcode", client.ContPostCode)
-                ),  // PostalAddress
-                new XElement("StreetAddress",
-                new XElement("Address1", addr1),
-                new XElement("Address2", addr2),
-                new XElement("Suburb", client.ContTownCity),
-                new XElement("State", client.ContCounty),
-                new XElement("Postcode", client.ContPostCode)
-                ),  // StreetAddresss
-                new XElement("Telephone", client.ContPhone),
-                new XElement("Fax", client.ContFax),
-                new XElement("Email", client.ContEmail),
-                new XElement("FSRAType"),
-                new XElement("BankAccountNumber"),
-                new XElement("BankName"),
-                new XElement("BankBranch"),
-                new XElement("CreditCardToken"),
-                new XElement("CreditCardExpiry"),
-                new XElement("Contact",
-                new XElement("Name", client.PrimName),
-                new XElement("Telephone", client.PrimPhone),
-                new XElement("Mobile", client.PrimMobile),
-                new XElement("Fax", client.PrimFax),
-                new XElement("Email", client.PrimEmail)
-                )   // Contact
-                ),  // Client
-                 new XElement("InvoiceDetails",
-                 new XElement("InvoicesTotal", invtotal),
-                 new XElement("InvoiceNumbers", invnumbers)
-                 )   // Invoices
-                )));
-            return request.ToString();
-        }
-
-        /// <summary>
         /// Posts an XML Message to Quick Fee
         /// </summary>
         /// <param name="client"></param>
@@ -171,93 +108,7 @@ namespace PEPaymentProvider
             var request = await client.PostAsync(String.Empty, new StringContent(xml)).ConfigureAwait(false);
             request.EnsureSuccessStatusCode();
             var loginResultXML = await request.Content.ReadAsStringAsync().ConfigureAwait(false);
-            return XDocument.Parse(loginResultXML);           
-        }
-
-        /// <summary>
-        /// Returns the FundML Session Id
-        /// </summary>
-        /// <param name="responseXML"></param>
-        /// <returns></returns>
-        private string GetSession(XDocument responseXML)
-        {
-            return responseXML.Descendants("Session").First().Value;
-        }
-
-        /// <summary>
-        /// Determines if the response was a FundML Success Result
-        /// </summary>
-        /// <param name="responseXML"></param>
-        /// <returns></returns>
-        private bool IsSuccess(XDocument responseXML)
-        {
-            var result = responseXML.Descendants("Result").First().Value;
-            if (result.Equals("Success", StringComparison.OrdinalIgnoreCase))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Produces an Error from the response XML
-        /// </summary>
-        /// <param name="responseXML"></param>
-        /// <returns></returns>
-        private ProviderError GetError(XDocument responseXML)
-        {
-            var result = responseXML.Descendants("Error").First();
-            return ProviderResult.ErrorResult(
-                result.Descendants("Code").First().Value,
-                result.Descendants("Message").First().Value);
-        }
-
-        /// <summary>
-        /// Produces an Error from the response XML
-        /// </summary>
-        /// <param name="responseXML"></param>
-        /// <returns></returns>
-        private ProviderRedirectResult GetRedirect(XDocument responseXML)
-        {
-            var result = responseXML.Descendants("Response").First();
-            var uri = new Uri(result.Descendants("URL").First().Value);
-            return ProviderResult.RedirectResult(uri);
-        }
-
-        /// <summary>
-        /// Builds the Login XML
-        /// </summary>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        private string buildLoginRequest(Config config)
-        {
-            var request = new XDocument(
-                new XElement("FundML",
-                new XElement("Version", 1),
-                new XElement("Request",
-                new XElement("Code", "LogOn"),
-                new XElement("Broker",
-                new XElement("Username", config.Username),
-                new XElement("Password", config.Password)
-                ))));
-            return request.ToString();
-        }
-
-        /// <summary>
-        /// Builds the Login XML
-        /// </summary>
-        /// <param name="config"></param>
-        /// <returns></returns>
-        private string buildLogoffRequest(string session)
-        {
-            var request = new XDocument(
-                new XElement("FundML",
-                new XElement("Version", 1),
-                new XElement("Request",
-                new XElement("Code", "LogOff"),
-                new XElement("Session", session)
-                )));
-            return request.ToString();
+            return XDocument.Parse(loginResultXML);
         }
 
         /// <summary>
@@ -266,7 +117,7 @@ namespace PEPaymentProvider
         /// <returns></returns>
         public IRegisteredObject GetHostedService()
         {
-            return null;
+            return new ReceiptingService();
         }
     }
 }
---
+++ src/PEPaymentProvider.QuickFee/Receipting/BankTrailerRecord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEPaymentProvider.Receipting
+{
+    public class BankTrailerRecord
+    {
+        public BankTrailerRecord(string record)
+        {
+            if (String.IsNullOrWhiteSpace(record))
+                throw new ArgumentException(nameof(record), "Trailer Record is Empty.");
+            if (record.Length != 219)
+                throw new ArgumentException(nameof(record), "Trailer Record is wrong length.");
+
+            if (record.Substring(0, 2) != "99")
+                throw new ArgumentOutOfRangeException(nameof(record), "data does not indicate this is a trailer record.");
+
+            BillerCode = record.Substring(2, 10);
+            NumPayments = int.Parse(record.Substring(12, 9));
+            TotalPayments = (decimal)int.Parse(record.Substring(21,15)) / 100M;
+            NumErrorCorrections = int.Parse(record.Substring(36, 9));
+            TotalErrorCorrections = (decimal)int.Parse(record.Substring(45, 15)) / 100M;
+            NumReversals = int.Parse(record.Substring(60, 9));
+            TotalReversals = (decimal)int.Parse(record.Substring(69, 15)) / 100M;
+            SettlementAmount = (decimal)int.Parse(record.Substring(84, 15)) / 100M;
+        }
+
+        public string BillerCode { get; private set; }
+
+        public int NumPayments { get; private set; }
+
+        public decimal TotalPayments { get; private set; }
+
+        public int NumErrorCorrections { get; private set; }
+
+        public decimal TotalErrorCorrections { get; private set; }
+
+        public int NumReversals { get; private set; }
+
+        public decimal TotalReversals { get; private set; }
+
+        public decimal SettlementAmount { get; private set; }
+    }
+}
