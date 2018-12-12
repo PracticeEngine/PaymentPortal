2ff6c2b419ced690cb9abb05969fea97d76dc46e
Andrew Hambly <ahambly@practice-engine.com>
2018-12-12 09:07:41 +0000

    Update Invoice Detail model to include Transaction Type and Currency Symbol Update Select Invoice view to use new model fields.

--- sql/PaymentPortal/PP_Invoice_Details.sql
+++ sql/PaymentPortal/PP_Invoice_Details.sql
@@ -9,11 +9,15 @@ CREATE PROCEDURE PP_Invoice_Details
 
 AS 
 
-SELECT D.DebtTranIndex, D.DebtTranDate, D.DebtTranRefAlpha, D.ClientCode, D.DebtTranName, D.DebtForTotal, D.DebtForUnpaid, D.DebtTranCur
+SELECT D.DebtTranIndex, T.TransTypeDescription As TransactionType, D.DebtTranDate, D.DebtTranRefAlpha, D.ClientCode, D.DebtTranName, D.DebtForTotal, D.DebtForUnpaid, D.DebtTranCur, C.CurSymbol
 FROM tblTranDebtor D
+INNER JOIN tblTranTypes T ON D.DebtTranType = T.TransTypeIndex
+INNER JOIN tblCurrency C ON D.DebtTranCur = C.Currency
 WHERE D.ContIndex = @ContIndex AND D.DebtForUnpaid <> 0 AND D.DebtTranType IN (3,4)
 UNION ALL
-SELECT D.DebtTranIndex, D.DebtTranDate, 'FC' + LTRIM(RTRIM(STR(DebtTranIndex))), D.ClientCode, D.DebtTranName, D.DebtForTotal, D.DebtForUnpaid, D.DebtTranCur
+SELECT D.DebtTranIndex, T.TransTypeDescription As TransactionType, D.DebtTranDate, 'FC' + LTRIM(RTRIM(STR(DebtTranIndex))), D.ClientCode, D.DebtTranName, D.DebtForTotal, D.DebtForUnpaid, D.DebtTranCur, C.CurSymbol
 FROM tblTranDebtor D
+INNER JOIN tblTranTypes T ON D.DebtTranType = T.TransTypeIndex
+INNER JOIN tblCurrency C ON D.DebtTranCur = C.Currency
 WHERE D.ContIndex = @ContIndex AND D.DebtForUnpaid <> 0 AND D.DebtTranType IN (10)
 ORDER BY DebtTranDate ASC
